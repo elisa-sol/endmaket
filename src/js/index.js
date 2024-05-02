f4e0e1c5cc1c7db3cca239820534181a18ae619f
elisa-sol <elizaveta-2002@bk.ru>
2024-05-02 15:45:26 +0300

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,36 @@
 import "../scss/style.scss";
-import "../fonts/TTLakes/stylesheet.css";
-import "./scripts/slider.js";
-import "./test.js";
+
+// import Swiper from "./swiper/swiper-bundle.min";
+// import "swiper/css";
+
+let init = false;
+let swiper;
+
+function swiperCard() {
+  if (window.innerWidth < 768) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper(".swiper", {
+        loop: true,
+        speed: 1000,
+        freeMode: true,
+        slidesPerView: 1.2,
+        spaceBetween: 10,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    }
+  } else if (init) {
+    swiper.destroy();
+    init = false;
+  }
+}
+
+swiperCard();
+window.addEventListener("resize", swiperCard);
+
+// import "../fonts/TTLakes/stylesheet.css";
+// import "./scripts/slider.js";
+// import "./test.js";
